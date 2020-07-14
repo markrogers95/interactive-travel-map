d66b896d29f30732bc3ab083c7e5e72c4c3d0621
Mark Rogers <mark_199518@hotmail.co.uk>
2020-07-14 20:50:03 +0100

    Adding post route

--- server/src/api/logEntry.js
+++ server/src/api/logEntry.js
@@ -8,4 +8,8 @@ router.get('/', (req, res) => {
     });
 });
 
-module.exports = router;
+router.post('/', (req, res) => {
+    console.log(req.body);
+});
+
+module.exports = router;  
--- server/src/index.js
+++ server/src/index.js
@@ -9,17 +9,20 @@ require('dotenv').config();
 const middlewares = require('./middlewares');
 
 const logs = require('./api/logEntry')
-mongoose.connect('process.env.DATABASE_URL', {
-    useNewUrlParser: true,
-});
 
 const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.use(morgan('common'));
 app.use(helmet());
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
 }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.json({
@@ -35,5 +38,4 @@ app.use(middlewares.errorHandler);
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`);
-});
-
+});   
